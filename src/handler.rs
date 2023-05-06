02b8e6dad4deed6b5cf82697348f41cac23559ed
lesleyrs <19632758+lesleyrs@users.noreply.github.com>
2023-05-06 16:47:16 +0200

    add dependencies

---
+++ src/handler.rs
@@ -0,0 +1,30 @@
+use crate::app::{App, AppResult};
+use crossterm::event::{KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
+
+/// Handles the key events and updates the state of [`App`].
+pub fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<()> {
+    if key_event.kind == KeyEventKind::Press {
+        match key_event.code {
+            // Exit application on `ESC` or `q`
+            KeyCode::Esc | KeyCode::Char('q') => {
+                app.quit();
+            }
+            // Exit application on `Ctrl-C`
+            KeyCode::Char('c') | KeyCode::Char('C') => {
+                if key_event.modifiers == KeyModifiers::CONTROL {
+                    app.quit();
+                }
+            }
+            // Counter handlers
+            KeyCode::Right => {
+                app.increment_counter();
+            }
+            KeyCode::Left => {
+                app.decrement_counter();
+            }
+            // Other handlers you could add here.
+            _ => {}
+        }
+    }
+    Ok(())
+}
